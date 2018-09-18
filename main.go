f767d9fcca22fad5997116fae36012799c45ef33
Carlos Monroy Fernández <cmonroy@boardfy.com>
2018-09-18 19:58:53 +0200

    Go course. Interfaces - Inmterfaced S06C54

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -13,13 +16,10 @@ func main() {
 	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
-}
 func (englishBot) getGreeting() string {
 	// very custom logic for generating an english greeting
 	return "Hi there!"
