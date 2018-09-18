d70f75e8018c16d80c0949df4ac5521373628de9
Carlos Monroy Fernández <cmonroy@boardfy.com>
2018-09-18 23:23:35 +0200

    Go course. Interfaces - writer interface implementation S06C67

--- main.go
+++ main.go
@@ -7,11 +7,20 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	res, err := http.Get("https://www.google.com")
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-	io.Copy(os.Stdout, res.Body)
+	lw := logWriter{}
+	q, _ := io.Copy(lw, res.Body)
+	fmt.Println("Impresos: ", q)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
 }
